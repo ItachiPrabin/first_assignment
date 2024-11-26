05d077b87cb2e915d749f090d200ab2a5626a318
ItachiPrabin <itachiprabin@gmail.com>
2024-11-26 14:02:49 +0545

    simple interest

---
+++ lib/view/simple_interest_view.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class SimpleInterest extends StatefulWidget {
+  const SimpleInterest({super.key});
+
+  @override
+  State<SimpleInterest> createState() => _SimpleInterestState();
+}
+
+class _SimpleInterestState extends State<SimpleInterest> {
+  double principle = 0;
+  double rate = 0;
+  double time = 0;
+  double result = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple Interest'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                principle = double.parse(value);
+              },
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Principal No',
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextField(
+              onChanged: (value) {
+                rate = double.parse(value);
+              },
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter rate:',
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextField(
+              onChanged: (value) {
+                time = double.parse(value);
+              },
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter time:',
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text('Result : $result',
+                style: const TextStyle(
+                  fontSize: 20,
+                )),
+            const SizedBox(
+              height: 8,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // State lai change gara
+                  // buil method ma feri jau ani refresh gara
+                  setState(() {
+                    result = (principle * rate * time) / 100;
+                  });
+                },
+                child: const Text('SimpleInterest Calculation'),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
